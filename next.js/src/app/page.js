8fbb1f1d0d486bfe254803aa7264841d322f03d4
Bogumił <hello@milewski.me>
2023-11-02 15:06:34 +0100

    added Benefits section to IndexPage

--- next.js/src/app/page.js
+++ next.js/src/app/page.js
@@ -1,15 +1,45 @@
 import Seo from "@/global/Seo";
 import fetchData from "@/utils/fetchData";
 import SchemaBreadcrumbs from "@/global/Schema/Breadcrumbs";
+import Hero from "@/components/sections/homepage-hero";
+import Benefits from "@/components/sections/homepage-benefits";
 
 // export const runtime = 'edge'
 
 const pathname = '';
 
 const IndexPage = async () => {
+  const { page: {
+    hero_Heading,
+    hero_Paragraph,
+    hero_Cta,
+    hero_Images,
+    hero_Videos,
+    benefits_Heading,
+    benefits_Paragraph,
+    benefits_List,
+    benefits_Paragraph2,
+    benefits_Cta,
+    benefits_Img,
+  }} = await getData();
+
   return (
     <>
-      <h1>Homepage</h1>
+      <Hero data={{
+        hero_Heading,
+        hero_Paragraph,
+        hero_Cta,
+        hero_Images,
+        hero_Videos,
+      }} />
+      <Benefits data={{
+        benefits_Heading,
+        benefits_Paragraph,
+        benefits_List,
+        benefits_Paragraph2,
+        benefits_Cta,
+        benefits_Img,
+      }} />
       <SchemaBreadcrumbs breadcrumbs={[
         { name: 'Main page', path: pathname },
       ]} />
@@ -29,8 +59,56 @@ export async function generateMetadata() {
 const getData = async () => {
   const { body: { data } } = await fetchData(`
     page: IndexPage(id: "indexPage") {
-        #Hero
+        # Hero
       hero_Heading
+      hero_Paragraph
+      hero_Cta {
+        theme
+        text
+        href
+      }
+      hero_Images {
+        asset {
+          altText
+          url
+          metadata {
+            lqip
+            dimensions {
+              width
+              height
+            }
+          }
+        }
+      }
+      hero_Videos {
+        asset {
+          url
+          altText
+        }
+      }
+        # Benefits
+      benefits_Heading
+      benefits_Paragraph
+      benefits_List
+      benefits_Paragraph2
+      benefits_Cta {
+        theme
+        text
+        href
+      }
+      benefits_Img {
+        asset {
+          altText
+          url
+          metadata {
+            lqip
+            dimensions {
+              width
+              height
+            }
+          }
+        }
+      }
     }
   `)
   return data;
---
+++ next.js/src/components/sections/homepage-hero/index.js
@@ -0,0 +1,39 @@
+import Markdown from '@/utils/Markdown';
+import styles from './styles.module.scss';
+import Button from '@/components/atoms/Button';
+import Img from '@/utils/Img';
+
+const Hero = ({
+  data: {
+    hero_Heading,
+    hero_Paragraph,
+    hero_Cta,
+    hero_Images,
+    hero_Videos,
+  }
+}) => {
+  return (
+    <section className={styles.wrapper}>
+      <header>
+        <Markdown.h1>{hero_Heading}</Markdown.h1>
+        <Markdown className={styles.paragraph}>{hero_Paragraph}</Markdown>
+        <div className="cta-wrapper">
+          {hero_Cta.map((cta, i) => (
+            <Button data={cta} key={i} />
+          ))}
+        </div>
+      </header>
+      {hero_Images.map((img, i) => (
+        <Img data={img} key={i} />
+      ))}
+      {hero_Videos.map(({ asset: { url, altText } }, i) => (
+        <video autoPlay loop muted controls key={i}>
+          <source src={url} />
+          <p>{altText}</p>
+        </video>
+      ))}
+    </section>
+  );
+};
+
+export default Hero;
