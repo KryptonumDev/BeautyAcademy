0041f30cdfd69771bc2bd7ff64232276101432f4
kryptonumstudio <kryptonumstudio@gmail.com>
2023-12-01 10:44:10 +0100

    add console logs

--- next.js/src/app/api/payment/intent-result-redirector/route.js
+++ next.js/src/app/api/payment/intent-result-redirector/route.js
@@ -3,8 +3,10 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
   const { searchParams } = new URL(req.url)
   let status = searchParams.get('status')
+  console.log(req.url)
+  console.log(status)
 
-  if(status === 'succeeded')
+  if (status === 'succeeded')
     return NextResponse.redirect(`https://beautyacademy.expert/payment-successful`)
 
   return NextResponse.redirect(`https://beautyacademy.expert/payment-failed`)
