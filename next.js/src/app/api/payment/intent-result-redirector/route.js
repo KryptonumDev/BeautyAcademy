3c3fd798c69dc29098a6f98cf6cae2b2a8008324
kryptonumstudio <kryptonumstudio@gmail.com>
2023-12-01 10:47:57 +0100

    repair redirector

--- next.js/src/app/api/payment/intent-result-redirector/route.js
+++ next.js/src/app/api/payment/intent-result-redirector/route.js
@@ -2,12 +2,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url)
-  let status = searchParams.get('status')
-  console.log(req.url)
-  console.log(status)
+  let redirect_status = searchParams.get('status')
 
-  if (status === 'succeeded')
+  if (redirect_status === 'succeeded')
     return NextResponse.redirect(`https://beautyacademy.expert/payment-successful`)
-
+  
   return NextResponse.redirect(`https://beautyacademy.expert/payment-failed`)
 }
+
